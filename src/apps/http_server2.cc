1c5f28e35ba81325beb184383cdca141350315e9
Matt McSorley <mcsorleymatt@gmail.com>
2013-09-23 14:32:23 -0400

    starting server2

--- src/apps/http_server2.cc
+++ src/apps/http_server2.cc
@@ -8,18 +8,20 @@
 #define FILENAMESIZE 100
 
 int handle_connection(int sock);
+long get_file_size(FILE * f);
 
 int main(int argc, char * argv[]) {
     int server_port = -1;
     int rc          =  0;
     int sock        = -1;
-
+    int res, s, c, accepted_sock;
+    struct sockaddr_in saddr;
     /* parse command line args */
     if (argc != 3) {
 	fprintf(stderr, "usage: http_server1 k|u port\n");
 	exit(-1);
     }
-
+    minet_init(MINET_KERNEL);
     server_port = atoi(argv[2]);
 
     if (server_port < 1500) {
@@ -28,19 +30,39 @@ int main(int argc, char * argv[]) {
     }
     
     /* initialize and make socket */
-
+    if((s = minet_socket(SOCK_STREAM)) < 0)
+    {
+        minet_perror("Socket Not Created");
+        
+    }
     /* set server address*/
 
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(server_port);
+   
     /* bind listening socket */
+    if (minet_bind(s, &saddr) < 0)
+    {
+        minet_perror("Socket could not bind");
+    }
 
     /* start listening */
+    if (minet_listen(s, 0) < 0)
+    {
+        minet_perror("Socket could not listen");
+    }
 
-    /* connection handling loop: wait to accept connection */
+    /* connection handling loop: wait to accept connection */    
 
-    while (1) {
+    while(1) {
 	
 	/* create read list */
-	
+	    if ((accepted_sock = minet_accept(s, &saddr)) > 0)
+        {
+            rc = handle_connection(accepted_sock);
+        }
 	/* do a select */
 	
 	/* process sockets that are ready */
@@ -49,13 +71,17 @@ int main(int argc, char * argv[]) {
 	
 	/* for a connection socket, handle the connection */
 	
-	rc = handle_connection(sock);
+	//rc = handle_connection(sock);
 	
     }
 }
 
-int handle_connection(int sock) {
+int handle_connection(int s) {
     bool ok = false;
+    char buf[BUFSIZE];
+    char filebuf[BUFSIZE];
+    char filename[BUFSIZE];
+    int res;
 
     char * ok_response_f = "HTTP/1.0 200 OK\r\n"	\
 	"Content-type: text/plain\r\n"			\
@@ -68,11 +94,59 @@ int handle_connection(int sock) {
 	"</body></html>\n";
     
     /* first read loop -- get request and headers*/
-
+    res = minet_read(s, buf, sizeof(buf)-1);
+    if (res > 0)
+    {
+        memset(filename, '\0', BUFSIZE);
+        filename[0] = buf[4];
+        int i = 5;
+        int j = 1;
+        while (buf[i] != 32)
+        {
+            filename[j] = buf[i];
+            i++;
+            j++;
+        }
+    }
+    else
+    {
+        perror("Error during read");
+    }
+    
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
 
     /* try opening the file */
+    FILE * fp;
+    
+    if ((fp = fopen(filename, "r")) == 0)
+    {
+        perror("File not opened");
+        minet_write(s, notok_response, strlen(notok_response)*sizeof(char));
+    }
+    else
+    {
+        ok = true;
+        long file_size = get_file_size(fp);
+        char ok_response_buf[BUFSIZE];
+        memset(ok_response_buf, '\0', BUFSIZE);
+        sprintf(ok_response_buf, ok_response_f, file_size);
+        /*printf("%s\n", ok_response_buf);
+        printf("%d\n", sizeof(ok_response_buf));*/
+        minet_write(s, ok_response_buf, strlen(ok_response_buf)*sizeof(char));
+        //send(s, ok_response_buf, sizeof(ok_response_buf), 0);
+        
+        memset(filebuf, '\0', BUFSIZE);
+        while(!feof(fp))
+        {
+            int num_read;
+
+            num_read = fread(filebuf, 1, BUFSIZE, fp);
+            minet_write(s, filebuf, num_read);
+            //send(s, filebuf, num_read, 0);
+            memset(filebuf, '\0', BUFSIZE);
+        }
+    }
 
     /* send response */
     if (ok) {
@@ -85,10 +159,26 @@ int handle_connection(int sock) {
     }
 
     /* close socket and free space */
-  
+    minet_close(s);
+    
     if (ok) {
 	return 0;
     } else {
 	return -1;
     }
+}
+
+
+long get_file_size(FILE * f)
+{
+    long size;
+    
+    fseek(f, 0, SEEK_END);
+    
+    size = ftell(f);
+    
+    fseek(f, 0, SEEK_SET);
+    
+    return size;
+    
 }
